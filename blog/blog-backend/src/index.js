a6d2e8a091c1ef3def2768b659f80cbbd3b14ce7
Lee Jaeyoon <leejaeyoon@Garcia.local>
2020-03-08 13:38:40 +0900

    change2

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,1 +1,11 @@
 const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx) => {
+    ctx.body = 'hello world';
+});
+
+app.listen(400, () => {
+    console.log('listening to port 4000');
+});
